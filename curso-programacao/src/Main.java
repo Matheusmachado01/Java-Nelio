e1c0f2a2dcd417ca83f20c232ed218f7d00beb02
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-07-18 22:57:57 -0300

    formate

--- curso-programacao/src/Main.java
+++ curso-programacao/src/Main.java
@@ -4,13 +4,26 @@ public class Main {
 	
 		public static void main(String[] args) {
 			
-			int y = 32;
-			double x = 10.35784;
+			String product1 = "Computer ";
+			String product2 = "Office desk";
+			int age = 30;
+			int code = 5290;
+			char gender = 'F';
+			double price1 = 2100.0;
+			double price2 = 650.50;
+			double measure = 53.234567;
 			
-	System.out.println(x);		
-	System.out.printf("%.2f%n", x);
+	System.out.println("Products: ");	
+	System.out.println("Computer, which is $ " + price1);
+	System.out.println("Office Desk, Wich price is $" + price2 );
+	System.out.println();
+	System.out.println("Record: "+ age + " years old, code " + code + " and gender: " + gender);
+	System.out.println();
+	System.out.printf("Measue with eight decimal places: %.8f%n", measure );
+	System.out.printf("Rouded (three decimal places): %.3f%n", measure );
 	Locale.setDefault(Locale.US);
-	System.out.println("RESULTADO = " + x + " METROS ");
+	System.out.printf("US decimal point: %.3f ", measure);
+	
 		}
 	
 	}
