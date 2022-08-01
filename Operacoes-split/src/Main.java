8e880586b74dc000b12cfa1933b89d83b6d17eb4
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-01 14:26:51 -0300

    Operacoes-split

---
+++ Operacoes-split/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String s = "potato apple lemon orange";
+		
+		String [] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+
+		
+	}
+
+}
