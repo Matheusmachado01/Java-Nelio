8ac20d23c4ec62040604a88bc5f12aab0611cbd9
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-04 22:06:26 -0300

    classe-representando- triangle

--- course/src/aplication/Program.java
+++ course/src/aplication/Program.java
@@ -3,31 +3,36 @@ package aplication;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double Xa, Xb, Xc, Ya, Yb, Yc;
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
 
 		System.out.println("Enter the measures of triangle X: ");
-		Xa = sc.nextDouble();
-		Xb = sc.nextDouble();
-		Xc = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		Ya = sc.nextDouble();
-		Yb = sc.nextDouble();
-		Yc = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 
-		double p = (Xa + Xb + Xc) / 2.0;
-		double areax = Math.sqrt(p * (p - Xa) * (p - Xb) * (p - Xc));
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areax = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-		p = (Ya + Yb + Yc) / 2.0;
-		double areay = Math.sqrt(p * (p - Ya) * (p - Yb) * (p - Yc));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areay = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
 		System.out.printf("Triangle X area: %.4f%n", areax);
-		System.out.printf("Triangle X area: %.4f%n", areay);
+		System.out.printf("Triangle Y area: %.4f%n", areay);
 
 		if (areax > areay) {
 			System.out.println("Larger area: X");
