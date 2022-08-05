a7eded147504afe3433ad6f3497e67f9b98de827
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-05 19:43:54 -0300

    Product

---
+++ orientacao_a_objetos_+_Projetos/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter Product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("quantity in stock: ");
+		product.quantity = sc.nextInt();
+
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+	
+		sc.close();
+	}
+
+}
