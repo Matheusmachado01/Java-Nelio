dfa419dae455352fe558ae5de3990a0ce74cfccf
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-11 19:08:30 -0300

    terceira-forma-sem-oo

--- circuference-volume-PI/src/application/Program.java
+++ circuference-volume-PI/src/application/Program.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 
 import util.Calculator;
 
+
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
+		
 
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = calc.circumference(radius);
+		double c = Calculator.circumference(radius);
 
-		double v = calc.voLume(radius);
+		double v = Calculator.voLume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("VoLume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 
 		sc.close();
 	}
