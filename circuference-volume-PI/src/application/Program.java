17f6b344de367da021a23567402ef6f7ffa1914a
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-11 18:56:16 -0300

    mdificado-para-oo-instanciado

--- circuference-volume-PI/src/application/Program.java
+++ circuference-volume-PI/src/application/Program.java
@@ -3,35 +3,28 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+import util.Calculator;
 
-	public static final double PI = 3.14159;
+public class Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Calculator calc = new Calculator();
 
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 
-		double v = voLume(radius);
+		double v = calc.voLume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("VoLume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", PI);
+		System.out.printf("PI value: %.2f%n", calc.PI);
 
 		sc.close();
-	}
-
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-
-	public static double voLume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
 }
