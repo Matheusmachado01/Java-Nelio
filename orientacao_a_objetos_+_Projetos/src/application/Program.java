d1217d1bd3d59c69225b7083bfd1a15fc5901d9d
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-16 17:44:20 -0300

    construtor_inserido

--- orientacao_a_objetos_+_Projetos/src/application/Program.java
+++ orientacao_a_objetos_+_Projetos/src/application/Program.java
@@ -12,21 +12,21 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		
+		Product product = new Product(name, price, quantity);
 		System.out.println();
 		System.out.println("Produtc Data: " + product);
 		
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
+		 quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
 		System.out.println();
--- orientacao_a_objetos_+_Projetos/src/entities/Product.java
+++ orientacao_a_objetos_+_Projetos/src/entities/Product.java
@@ -6,6 +6,13 @@ public class Product {
 	public double price;
 	public int quantity;
 
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	
 	public double totalValueInStock() {
 		return price * quantity;
 	}
