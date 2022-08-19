28bfd36417b9ade666fa564698fb42e8059f29ad
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-19 19:09:38 -0300

    Program

--- Account/src/application/Program.java
+++ Account/src/application/Program.java
@@ -1,10 +1,53 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		if (response == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+		}
+		else {
+			account = new Account(number, holder);
+		}
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a witdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data: ");
+		
+		
+		
+		sc.close();
 	}
 
 }
--- Account/src/entites/Account.java
+++ Account/src/entites/Account.java
@@ -2,4 +2,52 @@ package entites;
 
 public class Account {
 
+	private int number;
+	private String holder;
+	private double balance;
+
+	public Account(int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+	public Account(int number, String holder, double initialDeposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDeposit);
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount + 5.0;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	public String toString() {
+		return "Account "
+				+ number
+				+ ", Holder: "
+				+ holder
+				+", Balance: $ "
+				+ String.format("%.2f", balance);
+		
+		
+	}
 }
