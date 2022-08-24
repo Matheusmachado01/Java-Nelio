87e1329d7520013c84aa1c1e883d91388f5884a1
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-24 18:35:05 -0300

    Negativos

---
+++ Negativos/src/Program.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quantos números você vai digitar?");
+		int n = sc.nextInt();
+
+		int[] vect = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextInt();
+		}
+
+		System.out.println("NÚMEROS NEGATIVOS: ");
+
+		for (int i = 0; i < n; i++) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}
+		sc.close();
+	}
+
+}
