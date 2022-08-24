b61fdc2868d1550d6f9c60855987e545080e73f7
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-08-24 19:28:11 -0300

    Soma_vetor

--- Soma_vetor/src/Program.java
+++ Soma_vetor/src/Program.java
@@ -1,9 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n;
+		double soma, media;
+
+		System.out.print("Quantos números você vai digitar? ");
+		n = sc.nextInt();
+
+		double[] vect = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextDouble();
+		}
+		soma = 0;
+		for (int i = 0; i < n; i++) {
+			soma += vect[i];
+		}
+
+		media = soma / n;
+
+		System.out.println();
+		
+		System.out.print("VALORES = ");
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%.1f  ", vect[i]);
+		}
+		
+		System.out.printf("\nSOMA = %.1f ", soma);
+		System.out.printf("\nMEDIA = %.1f", media);
+	
+		sc.close();
 	}
 
 }
