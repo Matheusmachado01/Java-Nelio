fb33b35ee0876db606067ea106a13d28993fe644
matheus machado <matheus.machado0191@gmail.com>
2022-10-22 02:44:03 -0300

    aplicacao-com-2-classes-neste-diretorio

---
+++ Membos-Estáticos/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static final double PI = 3.14159;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc =  new Scanner (System.in);
+		System.out.println("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = circunference(radius);
+		
+		double v = volume(radius);
+		
+		System.out.printf("Circunference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI: %.2f%n", PI);
+
+		sc.close();
+	}
+
+	public static double circunference(double radius) {
+		return 2.0 * PI * radius;
+	}
+	public static double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
+}
