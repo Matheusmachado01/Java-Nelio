815041cb7905853332bd344e6ff2f86853aa921a
matheus machado <matheus.machado0191@gmail.com>
2022-10-29 09:00:41 -0300

    atualizado

--- Negativos/src/Program.java
+++ Negativos/src/Program.java
@@ -1,3 +1,4 @@
+import java.security.DrbgParameters.NextBytes;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,23 +9,25 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Quantos números você vai digitar? ");
+		System.out.print("QUANTOS NUMEROS VOCE VAI DIGITAR? ");
 		int n = sc.nextInt();
 		
-		int [] vect = new int[n];
+		int vect[] = new int[n];
 		
-		for (int i = 0; i < n; i++) {
-			System.out.print("Digite um número: ");
-			vect[i] = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			sc.nextLine();
+			System.out.print("Digite um numero: ");
+			 vect[i] = sc.nextInt();
 		}
-		System.out.println("NÚMEROS NEGATIVOS: ");
+		System.out.println("NUMEROS NEGATIVOS : ");
 		
-		for (int i = 0; i < n; i++) {
-		if	(vect [i] < 0) {
-			System.out.println(vect[i]);
+		
+		for(int i=0; i<n; i++) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
 		}
 		
-		}
 		sc.close();
 	}
 
