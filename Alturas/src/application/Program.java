b5f619a6351601f08b2fb915a96e33e5e0ae86d1
matheus machado <matheus.machado0191@gmail.com>
2022-10-31 16:38:53 -0300

    atualizado

--- Alturas/src/application/Program.java
+++ Alturas/src/application/Program.java
@@ -16,41 +16,40 @@ public class Program {
 		String nomes[] = new String[n];
 		int idades[] = new int[n];
 		double alturas[] = new double[n];
-
-		for (int i = 0; i < n; i++) {
-			System.out.println("Dados da " + (i + 1) + " pessoa: ");
-			System.out.print("Nome: ");
-			nomes[i] = sc.next();
-			System.out.print("Idade: ");
-			idades[i] = sc.nextInt();
-			System.out.print("Altura: ");
-			alturas[i] = sc.nextDouble();
+		
+		for(int i=0; i<n; i++) {
+		System.out.println("Dados da "+ (i+1) + "a pessoa: ");
+		System.out.print("Nome: ");
+		nomes[i] = sc.next();
+		System.out.print("Idade: ");
+		idades[i] = sc.nextInt();
+		System.out.print("Altura: ");
+		alturas[i] = sc.nextDouble();
 		}
-
+		
 		double soma = 0.0;
-		for (int i = 0; i < n; i++) {
+		for(int i=0; i<n; i++) {
 			soma = soma + alturas[i];
 		}
+		double mediaAlturas = soma / n;
 		System.out.println();
-		double alturaMedia = soma / n;
-		System.out.printf("Altura Média: %.2f%n", alturaMedia);
-
-		int cont = 0;
-		for (int i = 0; i < n; i++) {
-			if (idades[i] < 16) {
-				cont = cont + 1;
+		System.out.printf("Altura média: %.2f%n", mediaAlturas);
+		
+		int contMenores = 0;
+		for(int i=0; i<n; i++) {
+			if(idades[i] < 16) {
+				contMenores = contMenores +1;
 			}
 		}
-
-		double percentual = cont * 100.0 / n;
+		double percentual = contMenores * 100.0 / n;
 		System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percentual);
-
-		for (int i = 0; i < n; i++) {
-			if (idades[i] < 16) {
+		
+		for(int i=0; i<n; i++) {
+			if(idades[i] < 16) {
 				System.out.println(nomes[i]);
 			}
 		}
-
+		
 		sc.close();
 	}
 }
