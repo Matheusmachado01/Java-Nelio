49a4a8d7af23033d55f36aa44c115680563fd732
matheus machado <matheus.machado0191@gmail.com>
2022-11-16 15:35:59 -0300

    modificado

--- Exercicio-Employee/src/entities/Employee.java
+++ Exercicio-Employee/src/entities/Employee.java
@@ -6,13 +6,15 @@ public class Employee {
 	public double grossSalary;
 	public double tax;
 	
-	
-	
+
 	public double netSalary() {
-		return grossSalary - tax;
+	return grossSalary - tax;
 	}
 	public void increaseSalary(double percentage) {
-		grossSalary += grossSalary * percentage / 100.0;
+	grossSalary += grossSalary * percentage / 100.0;
+	}
+	public String toString() {
+	return name + ", $ " + String.format("%.2f", netSalary());
 	}
 	
 	
